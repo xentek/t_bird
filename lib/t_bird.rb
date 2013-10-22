e077270988aafa7b1c3eebd9a15270e55c2a5536
Eric Marden <eric@xentek.net>
2013-10-22 14:11:49 -0500

    requires gem's classes and adds basic error module

--- lib/t_bird.rb
+++ lib/t_bird.rb
@@ -1,4 +1,14 @@
-require "t_bird/version"
+# encoding: utf-8
+
+require 't_bird/configuration'
+require 't_bird/namer'
+require 't_bird/processor'
+require 't_bird/transmitter'
+require 't_bird/uploader'
+require 't_bird/version'
 
 module TBird
+  module Errors
+    class Unknown < StandardError ; end
+  end
 end
