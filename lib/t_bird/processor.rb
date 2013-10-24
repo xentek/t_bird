7d9850ce96ad3b0d6e008504b980476380e9b297
Eric Marden <eric@xentek.net>
2013-10-23 21:44:36 -0500

    improves Processor class in a number of ways
    - removes thumbnail_size attr_reader (let Configuration be the source of
      truth for that value)
    - improves variable naming, and makes it consistent
    - adds `stream` method for writing the image out to a StringIO stream
    - adds `original` method to provide noop processing of the image
    - updates and expands specs to keep up with changes

--- lib/t_bird/processor.rb
+++ lib/t_bird/processor.rb
@@ -4,34 +4,41 @@ require 'mini_magick'
 
 module TBird
   class Processor
-    attr_reader :image, :thumbnail_size
+    attr_reader :image
     def initialize(file_blob)
       @image = MiniMagick::Image.read(file_blob)
-      @thumbnail_size = Configuration.thumbnail_size
     end
 
     def process(&block)
-      image.combine_options do |magick|
-        block.call(magick) if block_given?
+      image.combine_options do |img|
+        block.call(img) if block_given?
       end
       image
     end
 
+    def stream
+      image.write StringIO.new
+    end
+    
+    def original
+      image # noop
+    end
+
     def resize(size)
-      process do |magick|
-        magick.resize size
+      process do |img|
+        img.resize size
       end
     end
 
-    def thumbnail
-      process do |magick|
-        magick.auto_orient
-        magick.thumbnail "x#{thumbnail_size*2}"
-        magick.resize "#{thumbnail_size*2}x<"
-        magick.resize "50%"
-        magick.gravity "center"
-        magick.crop "#{thumbnail_size}x#{thumbnail_size}+0+0"
-        magick.quality 92
+    def thumbnail(thumbnail_size = Configuration.thumbnail_size)
+      process do |img|
+        img.auto_orient
+        img.thumbnail "x#{thumbnail_size*2}"
+        img.resize "#{thumbnail_size*2}x<"
+        img.resize "50%"
+        img.gravity "center"
+        img.crop "#{thumbnail_size}x#{thumbnail_size}+0+0"
+        img.quality 92
       end
     end
   end
--- spec/t_bird/processor_spec.rb
+++ spec/t_bird/processor_spec.rb
@@ -6,38 +6,37 @@ describe TBird::Processor do
   include TBirdSpecData
 
   before do
-    @custom_process = Proc.new do |magick|
-                        magick.resize '50%'
-                        magick.quality 88
-                      end
-
+    @custom_process = ->(img) { img.quality 88 }
     @processor = TBird::Processor.new(upload_file)
-    @versions = {
-                  thumb: '90x90',
-                  medium: '300',
-                  large: 'x300',
-                  custom: @custom_process
-                }
   end
 
   it "can process an image" do
     image = @processor.process(&@custom_process)
-    image.write StringIO.new
     image.valid?.must_equal true
     image.destroy!
   end
 
-  it "can process image into a thubnail" do
+  it "can thumbnail an image" do
     image = @processor.thumbnail
-    image.write StringIO.new
     image.valid?.must_equal true
     image.destroy!
   end
 
   it "can resize an image" do
     image = @processor.resize('300')
-    image.write StringIO.new
     image.valid?.must_equal true
+    image.destroy!
+  end
+
+  it "can return the original image" do
+    image = @processor.original
+    image.valid?.must_equal true
+    image.destroy!
+  end
+
+  it "can write image to a stream" do
+    image = @processor.resize('x200')
+    @processor.stream.must_be_instance_of StringIO
     image.destroy!
   end
 end
