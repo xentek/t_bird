5736fe004ccdfde9db4ab9018bcef410accb5892
Eric Marden <eric@xentek.net>
2013-10-23 21:56:26 -0500

    fleshes out the Uploader and improves interfaces
    - moves content_type, and original_filename to attr_reader
    - removes unused original_file method
    - adds `Uploader.version` macro for registering file versions
    - adds `Uploader.versions` for returning a hash of the registered versions
    - adds `versions` instance method for reading `Uploader.versions`
    - adds `processor` and `namer` methods that return memoized instances of
      `Processor` and `Namer`, respectivley
    - adds `upload!` method to loop over `versions` and upload the processed
      files to s3, collecting the urls into `@uploads`
    - adds `uploads` attr_reader

--- lib/t_bird/uploader.rb
+++ lib/t_bird/uploader.rb
@@ -1,29 +1,59 @@
 # encoding: utf-8
 
 module TBird
-  class Upload
-    attr_reader :options
+  class Uploader
+    attr_reader :file, :options, :uploads, :content_type, :original_filename
     def initialize(file, options = {})
-      @options = default_options.merge(options)
       @file = file
+      @options = default_options.merge(options)
+      @uploads = {}
+      @content_type = @file.content_type
+      @original_filename = @file.original_filename
+      @options[:metadata].merge!(content_type: content_type)
     end
 
-    def content_type
-      @file_data ||= @file.content_type
+    def namer
+      @namer ||= Namer.new(original_filename, options[:identifier], options[:token])
     end
 
-    def original_filename
-      @file_data ||= @file.original_filename
+    def processor
+      @processor ||= Processor.new(@file)
     end
 
-    def original_file
-      @file_data ||= @file.read
+    def upload!
+      versions.each do |version,block|
+        block.call(processor)
+        transmission = Transmitter.new(namer.new_name(version), processor.stream, options[:metadata])
+        transmission.transmit!
+        @uploads[version] = transmission.url
+      end
+      uploads
+    end
+
+    def versions
+      self.class.versions
+    end
+
+    def self.versions
+      @versions ||= { 
+        thumbnail: ->(img) { img.thumbnail },
+        original: ->(img) { img.original }
+      }
+    end 
+
+    def self.version(name, &block)
+      @versions ||= {}
+      @versions[name.to_sym] = block
     end
 
     private
 
     def default_options
-      {}
+      {
+        identifier: nil,
+        token: nil,
+        metadata: {}
+      }
     end
   end
 end
--- spec/t_bird/uploader_spec.rb
+++ spec/t_bird/uploader_spec.rb
@@ -2,22 +2,55 @@
 
 require 'spec_helper'
 
-describe TBird::Upload do
+describe TBird::Uploader do
   include TBirdSpecData
 
   before do
-    @tbird_upload = TBird::Upload.new(upload_file)
+    @uploader = TBird::Uploader.new(upload_file)
+    @urls = ['https://s3.example.com/5a3f124f68b57512d3865da4c527f6b7bbd4173a/b54c0891-92a1-4641-8334-d396d5cc21c4_thumbnail.jpg',
+             'https://s3.example.com/5a3f124f68b57512d3865da4c527f6b7bbd4173a/b54c0891-92a1-4641-8334-d396d5cc21c4_original.jpg']
   end
 
-  it "can return original filename" do
-    @tbird_upload.original_filename.must_equal 'sample.jpg'
+  it "can return the file" do
+    @uploader.file.wont_be_nil
   end
 
-  it "can return content type" do
-    @tbird_upload.content_type.must_equal 'image/jpeg'
+  it "can return the options" do
+    @uploader.options.must_equal({ identifier: nil, token: nil, metadata: { content_type: "image/jpeg" } })
   end
 
-  it "can return the original file binary data" do
-    @tbird_upload.original_file.wont_be_nil
+  it "can return the content type" do
+    @uploader.content_type.must_equal 'image/jpeg'
+  end
+
+  it "can return the original filename" do
+    @uploader.original_filename.must_equal 'sample.jpg'
+  end
+
+  it "can return an instance of Processor" do
+    @uploader.processor.must_be_instance_of TBird::Processor
+  end
+  
+  it "can return an instance of Namer" do
+    @uploader.namer.must_be_instance_of TBird::Namer
+  end
+
+  it "can return the registered versions" do
+    @uploader.versions.must_equal TBird::Uploader.versions
+  end
+
+  it "can upload all versions" do
+    TBird::Transmitter.any_instance.stubs(:transmit!)
+    TBird::Transmitter.any_instance.stubs(:url).returns(*@urls)
+
+    @uploader.upload!.must_equal({ thumbnail: @urls.first, original: @urls.last })
+  end
+
+  it "can return the upload urls" do
+    TBird::Transmitter.any_instance.stubs(:transmit!)
+    TBird::Transmitter.any_instance.stubs(:url).returns(*@urls)
+
+    @uploader.upload!
+    @uploader.uploads.must_equal({ thumbnail: @urls.first, original: @urls.last })
   end
 end
